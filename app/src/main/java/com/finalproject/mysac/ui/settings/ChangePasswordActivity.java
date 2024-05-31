d8935d48269698a36945a2d2738088a1253e0ce5
SimonSimanjuntak11 <simonrisky11@gmail.com>
2024-06-01 05:40:37 +0700

    Update Halaman activity_edit_profil, activity_change_password.xml, fragment_settings.xml, SettingsFragment.java, ChangePasswordActivity.java

--- app/src/main/java/com/finalproject/mysac/ui/settings/ChangePasswordActivity.java
+++ app/src/main/java/com/finalproject/mysac/ui/settings/ChangePasswordActivity.java
@@ -1,13 +1,11 @@
 package com.finalproject.mysac.ui.settings;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.finalproject.mysac.R;
 
 public class ChangePasswordActivity extends AppCompatActivity {
